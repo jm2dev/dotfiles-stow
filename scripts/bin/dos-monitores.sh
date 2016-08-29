a2fff032044215dd31080075814027b26145076a
José Miguel <jm@0pt1mates.com>
2016-08-29 14:00:48 +0100

    conky, dual monitor i3; setxkbmap at zsh login.

---
+++ scripts/bin/dos-monitores.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+IN="LVDS1"
+EXT="HDMI1"
+
+if (xrandr | grep "${EXT} disconnected"); then
+    echo "xrandr --output $IN --auto $EXT --off"
+else
+    echo "xrandr --output $IN --auto $EXT --auto --above $IN"
+fi
