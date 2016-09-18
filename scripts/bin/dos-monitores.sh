428ef76580dc41a2fb8250e1d0dac3f451e2594a
José Miguel <jm@0pt1mates.com>
2016-09-18 22:00:27 +0100

    Fira font for st. Two displays shell script.

--- scripts/bin/dos-monitores.sh
+++ scripts/bin/dos-monitores.sh
@@ -4,7 +4,7 @@ IN="LVDS1"
 EXT="HDMI1"
 
 if (xrandr | grep "${EXT} disconnected"); then
-    echo "xrandr --output $IN --auto $EXT --off"
+    xrandr --output $IN --auto $EXT --off
 else
-    echo "xrandr --output $IN --auto --output $EXT --auto --above $IN"
+    xrandr --output $IN --auto --output $EXT --auto --above $IN
 fi
