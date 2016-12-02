204ce2db137b1a8e087129397fcd82607df74e5c
Jose Miguel <jose.carrasco@digital.hmrc.gov.uk>
2016-12-02 10:40:19 +0000

    Conky and scripts to manage external display.

--- scripts/bin/dos-monitores.sh
+++ scripts/bin/dos-monitores.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/bash
 
-IN="LVDS1"
-EXT="HDMI1"
+IN="eDP-1"
+EXT="HDMI-2"
+#IN="LVDS1"
+#EXT="HDMI1"
 
 if (xrandr | grep "${EXT} disconnected"); then
     xrandr --output $IN --auto $EXT --off
+    feh  --bg-fill /home/jm/Pictures/wallpapers/Fedora-Backgrounds-HD-12.jpg
 else
     xrandr --output $IN --auto --output $EXT --auto --above $IN
+    feh  --bg-fill /home/jm/Pictures/wallpapers/Fedora-Backgrounds-HD-12.jpg /home/jm/Pictures/wallpapers/Fedora-Backgrounds-HD-12.jpg
 fi
+
---
+++ scripts/bin/un-monitor.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+IN="eDP-1"
+EXT="HDMI-2"
+#IN="LVDS1"
+#EXT="HDMI1"
+
+#xrandr --output $IN --auto $EXT --off
+xrandr --output eDP-1 --auto --output HDMI-2 --off
+feh  --bg-fill /home/jm/Pictures/wallpapers/Fedora-Backgrounds-HD-12.jpg
+
